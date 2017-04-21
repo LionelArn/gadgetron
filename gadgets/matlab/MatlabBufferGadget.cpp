ada9c2dc60ec41dae75a556476e82c20b6f5e307
LionelArn <lionel.arn@epfl.ch>
2017-04-21 16:29:07 +0000

    debugging overflow

--- gadgets/matlab/MatlabBufferGadget.cpp
+++ gadgets/matlab/MatlabBufferGadget.cpp
@@ -29,8 +29,11 @@ int MatlabBufferGadget::process(GadgetContainerMessage<IsmrmrdReconData>* m1)
     GDEBUG("rbit_[0] size is %lu bytes\n",                  (long unsigned) sizeof(recon_data->rbit_[0]));
     GDEBUG("rbit_[0].data_ size is %lu bytes\n",            (long unsigned) sizeof(recon_data->rbit_[0].data_));
     GDEBUG("rbit_[0].data_.data_ size is %lu bytes\n",      (long unsigned) sizeof(recon_data->rbit_[0].data_.data_));
-    GDEBUG("rbit_[0].data_.data_[0] size is %lu bytes\n",   (long unsigned) sizeof(recon_data->rbit_[0].data_.data_[0]));
-    GDEBUG("rbit_[0].data_.data_[0]: (%f, %f)\n", real(recon_data->rbit_[0].data_.data_[0]),imag(recon_data->rbit_[0].data_.data_[0]));
+    //GDEBUG("rbit_[0].data_.data_[0] size is %lu bytes\n",   (long unsigned) sizeof(recon_data->rbit_[0].data_.data_[0]));
+    
+    for (int i = 0; i < 260; i++)
+        GDEBUG("rbit_[0].data_.data_[0]: (%f, %f)\n", real(recon_data->rbit_[0].data_.data_[i]),
+                                                      imag(recon_data->rbit_[0].data_.data_[i]));
     
     if(sizeof(recon_data->rbit_) < max_data_size) 
     {
