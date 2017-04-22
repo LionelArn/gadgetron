e3e8d3a0efa5aec0018e68a27a545536b64195a0
LionelArn <lionel.arn@epfl.ch>
2017-04-22 18:51:31 +0000

    debugging overflow

--- gadgets/matlab/MatlabBufferGadget.cpp
+++ gadgets/matlab/MatlabBufferGadget.cpp
@@ -63,7 +63,7 @@ int MatlabBufferGadget::process(GadgetContainerMessage<IsmrmrdReconData>* m1)
         // and reference.data) into n_packets in the RO dimension. After all
         // packets are sent, MATLAB reconcatenates everything.
         
-        int n_packets = ceil( float(data_bytes) / float(max_data_size) );
+        int n_packets = 16;//ceil( float(data_bytes) / float(max_data_size) );
         
         GDEBUG("Bucket size limit reached, parsing it into %i packets.\n", n_packets);
         
@@ -105,73 +105,6 @@ int MatlabBufferGadget::process(GadgetContainerMessage<IsmrmrdReconData>* m1)
                                         recon_data->rbit_[i].data_.data_.get_size(5)*
                                         recon_data->rbit_[i].data_.data_.get_size(6);
                 
-                //GDEBUG("Dim 1 element bytes: %lu\n", (long unsigned) bytes_dim_1);
-                /*
-                std::complex<float> packet  [end-beg]
-                                            [recon_data->rbit_[i].data_.data_.get_size(1)]
-                                            [recon_data->rbit_[i].data_.data_.get_size(2)]
-                                            [recon_data->rbit_[i].data_.data_.get_size(3)]
-                                            [recon_data->rbit_[i].data_.data_.get_size(4)]
-                                            [recon_data->rbit_[i].data_.data_.get_size(5)]
-                                            [recon_data->rbit_[i].data_.data_.get_size(6)];
-                 std::copy(  &(recon_data->rbit_[i].data_.data_[0]) + beg*bytes_dim_1,
-                             &(recon_data->rbit_[i].data_.data_[0]) + end*bytes_dim_1,
-                             (packet[0][0][0][0][0][0][0]));
-                 */
-                
-                /*
-                mwSize ndim = input->get_number_of_dimensions();
-                mwSize* dims = new mwSize[ndim];
-                for (size_t i = 0; i < ndim; i++)
-                    dims[i] = input->get_size(i);
-                T* raw_data = (T*) mxCalloc(input->get_number_of_elements(),sizeof(T));
-                memcpy(raw_data,input->get_data_ptr(),input->get_number_of_bytes());
-                auto result =  mxCreateNumericMatrix(0,0,MatlabClassID<T>::value,isComplex<T>::value);
-                mxSetDimensions(result,dims,ndim);
-                mxSetData(result,raw_data);
-                return result;
-                */
-                
-                // for some reason, this methods only sends some weird real data to matlab.
-                /*
-                std::complex<float> * packet = (std::complex<float> *) mxCalloc((end-beg+1)*(bytes_dim_1/sizeof(std::complex<float>)), sizeof(std::complex<float>));
-                memcpy(&(packet[0]), &(recon_data->rbit_[i].data_.data_[beg]),  (end-beg+1)*bytes_dim_1);
-                
-                // convert the packet to MALTAB array
-                mwSize packet_ndim = recon_data->rbit_[i].data_.data_.get_number_of_dimensions();
-                mwSize* packet_dims = new mwSize[packet_ndim];
-                packet_dims[0] = end-beg+1;
-                for (size_t j = 1; j < packet_ndim; j++)
-                    packet_dims[j] = recon_data->rbit_[i].data_.data_.get_size(j);
-
-                auto mxdata =  mxCreateNumericMatrix(0, 0, mxSINGLE_CLASS, mxCOMPLEX);
-                mxSetDimensions(mxdata,packet_dims,packet_ndim);
-                mxSetData(mxdata,packet);
-                */
-                
-                
-                /*
-                size_t ndim = input->get_number_of_dimensions();
-                mwSize* dims = new mwSize[ndim];
-                for (size_t i = 0; i < ndim; i++)
-                    dims[i] = input->get_size(i);
-
-                REAL* real_data = (REAL*) mxCalloc(input->get_number_of_elements(),sizeof(REAL));
-                REAL* imag_data = (REAL*) mxCalloc(input->get_number_of_elements(),sizeof(REAL));
-
-                complext<REAL>* raw_data = input->get_data_ptr();
-                for (size_t i = 0; i < input->get_number_of_elements(); i++){
-                    real_data[i] = real(raw_data[i]);
-                    imag_data[i] = imag(raw_data[i]);
-                }
-
-                auto result  =  mxCreateNumericMatrix(0,0,MatlabClassID<REAL>::value,isComplex<complext<REAL>>::value);
-                mxSetDimensions(result,dims,ndim);
-                mxSetData(result,real_data);
-                mxSetImagData(result,imag_data);
-
-                auto ndims_test = mxGetNumberOfDimensions(result);
-                */
                 
                 size_t packet_n_elem = (end-beg+1) * dim_1_n_elem;
                 
@@ -214,32 +147,11 @@ int MatlabBufferGadget::process(GadgetContainerMessage<IsmrmrdReconData>* m1)
                 mxSetData      (mxdata, real_data);
                 mxSetImagData  (mxdata, imag_data);
                 
+                // amazingly, sending the data to MATLAB is the slowest operation
+                // of this for loop
                 GDEBUG("Sending data packet #%i...\n", p+1);
-                
                 std::string cmd = "data_" + std::to_string(i) + "_" + std::to_string(p);
                 engPutVariable(engine_, cmd.c_str(), mxdata);
-                
-                
-                
-                /*
-                std::string wcmd = "fprintf(2,evalc('whos'))";
-                send_matlab_command(wcmd);
-                */
-                
-                /*
-                wcmd = "fprintf(2,evalc(' [" + cmd + "(1:10,1,1,1)] '))";
-                send_matlab_command(wcmd);
-                
-                wcmd = "fprintf(2,evalc(' [" + cmd + "(1,1,1,1:10)] '))";
-                send_matlab_command(wcmd);
-                */
-                
-                /*
-                wcmd = "figure; imagesc(abs(squeeze(" + cmd + "(round(size(" + cmd + ",1)/2),:,:,1)))); drawnow;"+
-                       "figure; imagesc(abs(squeeze(" + cmd + "(:,128,:,1)))); drawnow;"+
-                       "figure; imagesc(abs(squeeze(" + cmd + "(:,:,128,1)))); drawnow; pause(10);";
-                send_matlab_command(wcmd);
-                */
                 
                 /*
                 // do the same for the reference
